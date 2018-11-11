5929624345e71011af939c0c334691791c95e4bc
Shaun Irwin <shaun.irwin@gmail.com>
2018-11-11 17:46:50 +0200

    Pass event data folders rather than files to main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import argparse
+import os
+import glob
 
 from event_Python import eventvision
 from lib.spatio_temporal_feature import TimeSurface
@@ -9,12 +11,23 @@ from lib.utils import cosine_dist, euclidean_dist
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train digit recogniser')
-    parser.add_argument('--input_files', action='store', nargs='+', default='datasets/mnist/Test/0/00004.bin',
-                        help='Path to event file')
+    parser.add_argument('--input_folders_training', action='store', nargs='+', default='datasets/mnist/Test/0',
+                        help='Paths to folders containing event files')
+    parser.add_argument('--num_files_per_folder', action='store', default=1, type=int,
+                        help="Number of files to read in from each digit's folder")
 
     args = parser.parse_args()
 
-    ev = eventvision.read_dataset(args.input_files[0])
+    # get event data files within folders
+
+    input_files_all = []
+
+    for folder in args.input_folders_training:
+        input_files = glob.glob(os.path.join(folder, '*.bin'))[:args.num_files_per_folder]
+        input_files_all.extend(input_files)
+        print('Num files from {}: {}'.format(folder, len(input_files)))
+
+    ev = eventvision.read_dataset(input_files_all[0])
 
     # #############   plot time surface for whole event sequence  ###############
 
@@ -33,18 +46,19 @@ if __name__ == '__main__':
             else:
                 ts_1_2.process_event(e)
 
-    fig, ax = plt.subplots(2, 3, figsize=(10, 5))
-    ax[0, 0].imshow(ts_1_1.latest_times)
-    ax[0, 1].imshow(ts_1_1.time_context)
-    ax[0, 2].imshow(ts_1_1.time_surface)
-    ax[1, 0].imshow(ts_1_2.latest_times)
-    ax[1, 1].imshow(ts_1_2.time_context)
-    ax[1, 2].imshow(ts_1_2.time_surface)
-    ax[0, 0].set_title('Latest times')
-    ax[0, 1].set_title('Time context')
-    ax[0, 2].set_title('Time surface')
+    if True:
+        fig, ax = plt.subplots(2, 3, figsize=(10, 5))
+        ax[0, 0].imshow(ts_1_1.latest_times)
+        ax[0, 1].imshow(ts_1_1.time_context)
+        ax[0, 2].imshow(ts_1_1.time_surface)
+        ax[1, 0].imshow(ts_1_2.latest_times)
+        ax[1, 1].imshow(ts_1_2.time_context)
+        ax[1, 2].imshow(ts_1_2.time_surface)
+        ax[0, 0].set_title('Latest times')
+        ax[0, 1].set_title('Time context')
+        ax[0, 2].set_title('Time surface')
 
-    plt.show()
+        plt.show()
 
     # ############## Initialise time surface prototypes ##############
 
@@ -64,13 +78,19 @@ if __name__ == '__main__':
             x = ev.data[i].x
             y = ev.data[i].y
 
-            C_1_on[i][y, x] = 1
+            C_1[i][y, x] = 1
     elif False:
         for i in range(N_1):
             x = ev.width / (N_1 + 1) * (i + 1)
             y = ev.height / (N_1 + 1) * (i + 1)
 
             C_1[i][y, x] = 1
+    elif False:
+        # hard-coded locations
+        C_1[0][12, 11] = 1
+        C_1[1][13, 16] = 1
+        C_1[2][26, 8] = 1
+        C_1[3][24, 17] = 1
     else:
         for i in range(N_1):
             x = ev.data[i].x
@@ -80,26 +100,26 @@ if __name__ == '__main__':
 
             C_1[i] = S_init.time_surface
 
-    fig, ax = plt.subplots(1, N_1, figsize=(25, 5))
+    if True:
+        fig, ax = plt.subplots(1, N_1, figsize=(25, 5))
 
-    for i in range(N_1):
-        ax[i].imshow(C_1[i])
-        ax[i].set_title('Time surface {}'.format(i))
+        for i in range(N_1):
+            ax[i].imshow(C_1[i])
+            ax[i].set_title('Time surface {}'.format(i))
 
-    plt.show()
+        plt.show()
 
     # ############ Train time surface prototypes for layer 1 ############
 
     event_data = []
 
-    for f in args.input_files:
+    for f in input_files_all:
         event_data.extend(eventvision.read_dataset(f).data)
 
     # initialise time surface
     S_on = TimeSurface(ev.height, ev.width, region_size=r_1, time_constant=tau_1)
     S_off = TimeSurface(ev.height, ev.width, region_size=r_1, time_constant=tau_1)
 
-    # TODO: should we have a p_on and p_off, to count separately for each prototype?
     p = [1] * N_1
 
     for e in event_data: #[:20]:
@@ -110,9 +130,6 @@ if __name__ == '__main__':
         else:
             S_off.process_event(e)
             S = S_off
-
-        # plt.imshow(S.time_surface_on)
-        # plt.show()
 
         # find closest cluster center (i.e. closest time surface prototype, according to euclidean distance)
 
@@ -138,6 +155,6 @@ if __name__ == '__main__':
 
     for i in range(N_1):
         ax[i].imshow(C_1[i])
-        ax[i].set_title('Time surface {}'.format(i))
+        ax[i].set_title('Layer 1. Time surface {} (p={})'.format(i, p[i]))
 
     plt.show()
