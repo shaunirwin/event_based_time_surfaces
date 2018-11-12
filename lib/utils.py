4d199d7a6d4533c7b5b3ea2e2b763a30e1524625
Shaun Irwin <shaun.irwin@gmail.com>
2018-11-12 21:21:52 +0200

    Add normalised euclidean and bhattaryya dist measures

--- lib/utils.py
+++ lib/utils.py
@@ -2,14 +2,43 @@ import numpy as np
 
 
 def cosine_dist(x, y):
-    """ Calculate cosine similarity between two matrices
+    """
+    Calculate cosine similarity between two vectors
     """
 
     return np.dot(x, y) / (np.sqrt(np.dot(x, x)) * np.sqrt(np.dot(y, y)))
 
 
 def euclidean_dist(x, y):
-    """ Calculate euclidean distance between two matrices
+    """
+    Calculate euclidean distance between two vectors
     """
 
     return np.sqrt(np.sum((x - y) ** 2))
+
+
+def euclidean_norm_dist(x, y):
+    """
+    Calculate euclidean distance, normalised by number of "features"
+    """
+
+    x = x.astype(float)
+    y = y.astype(float)
+
+    return np.sqrt(np.sum((x / x.sum() - y / y.sum()) ** 2))
+
+
+def bhattacharyya_dist(x, y):
+    """
+    Calculate Bhattacharyya distance, as explained in Bhattacharyya 1943 "On a measure of divergence between two
+    statistical populations defined by probability distributions".
+
+    This measure determines the relative closeness of two distributions, for example to measure separability between two
+    classes for classification. It is a generalisation of the Mahalanobis distance, for cases where the standard
+    deviation of the two classes are different.
+    """
+
+    x = x.astype(float)
+    y = y.astype(float)
+
+    return -np.log(np.sum(np.sqrt(np.multiply(x / x.sum(), y / y.sum()))))
--- unit_tests/test_utils.py
+++ unit_tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from lib.utils import cosine_dist, euclidean_dist
+from lib.utils import cosine_dist, euclidean_dist, euclidean_norm_dist, bhattacharyya_dist
 
 
 def test_cosine_dist_both_vectors_same_direction():
@@ -41,3 +41,35 @@ def test_euclidean_distance_for_different_vectors_gives_non_zero_scalar():
     b = np.array([0, 1])
 
     assert euclidean_dist(a, b) == np.sqrt(2)
+
+
+def test_normalised_euclidean_distance_for_same_vectors_gives_0():
+    a = np.array([2, 0])
+
+    b = np.array([1, 0])
+
+    assert euclidean_norm_dist(a, b) == 0
+
+
+def test_normalised_euclidean_distance_for_different_vectors_gives_gt_0():
+    a = np.array([2, 0])
+
+    b = np.array([0, 1])
+
+    assert euclidean_norm_dist(a, b) == np.sqrt(2)
+
+
+def test_bhattacharyya_distance_for_same_vectors_gives_0():
+    a = np.array([1, 2, 3])
+
+    b = np.array([1, 2, 3])
+
+    assert bhattacharyya_dist(a, b) == 0
+
+
+def test_bhattacharyya_distance_for_different_vectors_gives_gt_0():
+    a = np.array([1, 2, 3])
+
+    b = np.array([1, 2, 4])
+
+    assert bhattacharyya_dist(a, b) > 0
